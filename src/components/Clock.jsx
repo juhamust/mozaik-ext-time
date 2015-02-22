0a9fad8160b11b32fcfe075cd226324cd2aaa123
Juha Mustonen <juha.mustonen@iki.fi>
2015-02-22 09:30:49 +0200

    Added day/night indicator

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,4 +1,5 @@
 var React    = require('react');
+var addons   = require('react-addons');
 var d3       = require('d3');
 var moment   = require('moment');
 var timezone = require('moment-timezone');
@@ -25,12 +26,15 @@ var Clock = React.createClass({
     },
 
     componentDidMount() {
+        console.log('huurs', this.state.hours);
+
         setInterval(() => {
             this.setState(getCurrentTimeParts(this.props.timezone));
         }, 1000);
     },
 
     render() {
+        var cs = addons.classSet;
         var hoursStyle   = {
             transform: 'rotate(' + hoursScale(this.state.hours % 12) + 'deg)'
         };
@@ -43,10 +47,27 @@ var Clock = React.createClass({
 
         var brand = this.props.timezone || 'mozaïk';
 
+        brand = this.state.hours;
+
+        var isDay = true;
+        if (this.state.hours >= 18 &&  this.state.hours < 6) {
+            isDay = false;
+        }
+
+        var timeIndicatorClasses = cs({
+            'time__clock__indicator': true,
+            'fa': true,
+            'fa-sun-o': isDay,
+            'fa-moon-o': !isDay
+        });
+
+        //window.alert('HOUR:' + this.state.hours);
+
         return (
             <div>
                 <div className="time__clock__outer-circle" />
                 <span className="time__clock__brand">{brand}</span>
+                <span className={timeIndicatorClasses}></span>
                 <div className="time__clock__hand time__clock__hand--seconds" style={secondsStyle} />
                 <div className="time__clock__hand time__clock__hand--minutes" style={minutesStyle} />
                 <div className="time__clock__hand time__clock__hand--hours"   style={hoursStyle} />
